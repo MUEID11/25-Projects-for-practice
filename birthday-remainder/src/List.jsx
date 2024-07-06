76301aacce62176f3cd9955a91c06af4bf681147
MUEID11 <mehedirangpur3@gmail.com>
2024-07-06 23:49:20 +0600

    half way

---
+++ birthday-remainder/src/List.jsx
@@ -0,0 +1,12 @@
+
+
+const List = () => {
+    return (
+        <div className="">
+            <h6 className="text-xl font-medium">List here</h6>
+            <button className="btn bg-green-700 text-white px-4 py-2 rounded" onClick={() => console.log('clicked')}>Clear</button>
+        </div>
+    );
+};
+
+export default List;
---
+++ birthday-remainder/src/data.js
@@ -0,0 +1,123 @@
+[
+    {
+      "id": 1,
+      "name": "Bertie Yates",
+      "age": 29,
+      "image": "https://res.cloudinary.com/diqqf3eq2/image/upload/v15995959131/person-2_ipcjws.jpg"
+    },
+    {
+      "id": 2,
+      "name": "Jane Doe",
+      "age": 25,
+      "image": "https://res.cloudinary.com/diqqf3eq2/image/upload/v15995959131/person-2_ipcjws.jpg"
+    },
+    {
+      "id": 3,
+      "name": "John Smith",
+      "age": 34,
+      "image": "https://res.cloudinary.com/diqqf3eq2/image/upload/v15995959131/person-2_ipcjws.jpg"
+    },
+    {
+      "id": 4,
+      "name": "Alice Johnson",
+      "age": 28,
+      "image": "https://res.cloudinary.com/diqqf3eq2/image/upload/v15995959131/person-2_ipcjws.jpg"
+    },
+    {
+      "id": 5,
+      "name": "Chris Evans",
+      "age": 40,
+      "image": "https://res.cloudinary.com/diqqf3eq2/image/upload/v15995959131/person-2_ipcjws.jpg"
+    },
+    {
+      "id": 6,
+      "name": "Linda Green",
+      "age": 30,
+      "image": "https://res.cloudinary.com/diqqf3eq2/image/upload/v15995959131/person-2_ipcjws.jpg"
+    },
+    {
+      "id": 7,
+      "name": "Michael Brown",
+      "age": 37,
+      "image": "https://res.cloudinary.com/diqqf3eq2/image/upload/v15995959131/person-2_ipcjws.jpg"
+    },
+    {
+      "id": 8,
+      "name": "Emily Davis",
+      "age": 22,
+      "image": "https://res.cloudinary.com/diqqf3eq2/image/upload/v15995959131/person-2_ipcjws.jpg"
+    },
+    {
+      "id": 9,
+      "name": "David Wilson",
+      "age": 45,
+      "image": "https://res.cloudinary.com/diqqf3eq2/image/upload/v15995959131/person-2_ipcjws.jpg"
+    },
+    {
+      "id": 10,
+      "name": "Jessica Taylor",
+      "age": 26,
+      "image": "https://res.cloudinary.com/diqqf3eq2/image/upload/v15995959131/person-2_ipcjws.jpg"
+    },
+    {
+      "id": 11,
+      "name": "Joshua Lee",
+      "age": 33,
+      "image": "https://res.cloudinary.com/diqqf3eq2/image/upload/v15995959131/person-2_ipcjws.jpg"
+    },
+    {
+      "id": 12,
+      "name": "Sarah Miller",
+      "age": 29,
+      "image": "https://res.cloudinary.com/diqqf3eq2/image/upload/v15995959131/person-2_ipcjws.jpg"
+    },
+    {
+      "id": 13,
+      "name": "Daniel Martinez",
+      "age": 38,
+      "image": "https://res.cloudinary.com/diqqf3eq2/image/upload/v15995959131/person-2_ipcjws.jpg"
+    },
+    {
+      "id": 14,
+      "name": "Sophia Anderson",
+      "age": 24,
+      "image": "https://res.cloudinary.com/diqqf3eq2/image/upload/v15995959131/person-2_ipcjws.jpg"
+    },
+    {
+      "id": 15,
+      "name": "Ryan Thomas",
+      "age": 35,
+      "image": "https://res.cloudinary.com/diqqf3eq2/image/upload/v15995959131/person-2_ipcjws.jpg"
+    },
+    {
+      "id": 16,
+      "name": "Olivia Jackson",
+      "age": 27,
+      "image": "https://res.cloudinary.com/diqqf3eq2/image/upload/v15995959131/person-2_ipcjws.jpg"
+    },
+    {
+      "id": 17,
+      "name": "Ethan White",
+      "age": 31,
+      "image": "https://res.cloudinary.com/diqqf3eq2/image/upload/v15995959131/person-2_ipcjws.jpg"
+    },
+    {
+      "id": 18,
+      "name": "Ava Harris",
+      "age": 23,
+      "image": "https://res.cloudinary.com/diqqf3eq2/image/upload/v15995959131/person-2_ipcjws.jpg"
+    },
+    {
+      "id": 19,
+      "name": "Matthew Clark",
+      "age": 36,
+      "image": "https://res.cloudinary.com/diqqf3eq2/image/upload/v15995959131/person-2_ipcjws.jpg"
+    },
+    {
+      "id": 20,
+      "name": "Isabella Lewis",
+      "age": 28,
+      "image": "https://res.cloudinary.com/diqqf3eq2/image/upload/v15995959131/person-2_ipcjws.jpg"
+    }
+  ]
+  
